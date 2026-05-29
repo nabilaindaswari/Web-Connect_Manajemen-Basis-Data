1480acc65c11056b81a8930c412de45e953f9635
RickProduction <untuksementara77@gmail.com>
2026-05-29 14:51:56 +0700

    Updating Proses_Transaksi Variables in Back-End

--- process/proses_transaksi.php
+++ process/proses_transaksi.php
@@ -2,20 +2,36 @@
 session_start();
 require_once '../config/database.php';
 
-$id_user          = $_SESSION['user_id'];
+$id_kasir        = $_SESSION['user_id'];
 $id_metode       = (int)($_POST['id_metode'] ?? 0);
 $total_bayar     = (int)($_POST['total_bayar'] ?? 0);
 $total_harga     = (int)($_POST['total_harga'] ?? 0);
 
+# Pembuatan JSON Keranjang berasal dari ID Barang dan Jumlah yang dikirim dari form checkout
+
+$p_keranjang     = [];
+foreach ($_SESSION['keranjang'] as $item) {
+    $p_keranjang[] = [
+        'id_barang' => (int)$item['id_barang'],
+        'jumlah'    => (int)$item['jumlah_barang']
+    ];
+}
+$p_json_keranjang = json_encode($p_keranjang);
+
+
 /* Ambil nama_lengkap untuk ditampilkan di struk */
-$stmtUser = $pdo->prepare("SELECT nama_lengkap FROM users WHERE id_user = ? LIMIT 1");
-$stmtUser->execute([$id_user]);
+$stmtUser = $pdo->prepare("SELECT nama_lengkap FROM kasir WHERE id_kasir = ? LIMIT 1");
+$stmtUser->execute([$id_kasir]);
 $userRow       = $stmtUser->fetch(PDO::FETCH_ASSOC);
 $kasir = $userRow ? $userRow['nama_lengkap'] : $username_session;
 
-$stmtTransaksi = $pdo->prepare("CALL SP_Checkout_Kasir(:p_id_user, :p_id_metode, :p_total_bayar, :p_json_keranjang, @kembalian)");
+
+
+$stmtTransaksi = $pdo->prepare("CALL SP_Checkout_Kasir(:p_id_kasir, :p_id_metode, :p_total_bayar, :p_json_keranjang, @kembalian)");
+var_dump($id_metode);
+die();
 $stmtTransaksi->execute([
-    ':p_id_user'        => $id_user,
+    ':p_id_kasir'       => $id_kasir,
     ':p_id_metode'      => $id_metode,
     ':p_total_bayar'    => $total_bayar,
     ':p_json_keranjang' => $p_json_keranjang,
